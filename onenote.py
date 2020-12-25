52f2067309d093d97900038525da9570b724ed68
jarekj9 <jerry.esp.sha@gmail.com>
2020-12-26 00:52:49 +0100

    refactor

--- onenote.py
+++ onenote.py
@@ -14,13 +14,6 @@ from bs4 import BeautifulSoup
 from msal import PublicClientApplication, SerializableTokenCache
 from dateutil import parser
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('logfile.log')
-formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
 
 class OneNoteDownload:
     '''Can download notebook'''
@@ -29,7 +22,7 @@ class OneNoteDownload:
         self.AUTHORITY = 'https://login.microsoftonline.com/common'
         self.URL_SECTIONS = f'https://graph.microsoft.com/v1.0/users/{username}/onenote/sections'
         self.URL_PAGES = f'https://graph.microsoft.com/v1.0/users/{username}/onenote/pages'
-
+        self.logger = setup_logger()
         access_token = self.get_access_token()
         self.headers = {'Authorization': f'{access_token}'}
         self.section_ids = self._get_sections_ids()
@@ -79,26 +72,34 @@ class OneNoteDownload:
         while next_link:
             for attempt in range(3):
                 try:
-                    resp = requests.get(next_link, headers=self.headers)
-                    sections_response = json.loads(resp.text)
-                    for section_data in sections_response['value']:
-                        name = section_data["displayName"]
-                        if name in section_ids:  # if sections are duplicated
-                            existing_date = parser.isoparse(section_ids[name]["lastModifiedDateTime"])
-                            new_date = parser.isoparse(section_data["lastModifiedDateTime"])
-                            if new_date > existing_date:
-                                section_ids[name] = section_data
-                        else:
-                            section_ids[name] = section_data
+                    section_ids = self._get_section_ids_from_link(section_ids, next_link)
                 except Exception as e:
-                    logger.warning(e)
-                    logger.warning(resp.text)
-                    logger.warning(f'Retrying {attempt} time.')
+                    self.logger.warning(e)
+                    self.logger.warning(f'Retrying {attempt} time.')
                     time.sleep(2)
 
-            next_link = sections_response.get('@odata.nextLink')
+            sections_dict = self._get_sections_dict_from_link(next_link)
+            next_link = sections_dict.get('@odata.nextLink')
 
         return section_ids
+    
+    def _get_section_ids_from_link(self, section_ids, link):
+        sections_dict = self._get_sections_dict_from_link(link)
+        for section_data in sections_dict['value']:
+            name = section_data["displayName"]
+            if name in section_ids:  # if sections are duplicated
+                existing_date = parser.isoparse(section_ids[name]["lastModifiedDateTime"])
+                new_date = parser.isoparse(section_data["lastModifiedDateTime"])
+                if new_date > existing_date:
+                    section_ids[name] = section_data
+            else:
+                section_ids[name] = section_data
+        return section_ids
+
+    def _get_sections_dict_from_link(self, link):
+        resp = requests.get(link, headers=self.headers)
+        return json.loads(resp.text)
+
 
     def get_pages(self, section_name):
         '''Returns dict with mapping: {page title: page id}'''
@@ -117,9 +118,9 @@ class OneNoteDownload:
                         pages[pages_data.get("title")] = pages_data.get("id")
                     break
                 except Exception as e:
-                    logger.warning(e)
-                    logger.warning(resp.text)
-                    logger.warning(f'Retrying {attempt} time.')
+                    self.logger.warning(e)
+                    self.logger.warning(resp.text)
+                    self.logger.warning(f'Retrying {attempt} time.')
                     time.sleep(2)
 
             next_link = pages_response.get('@odata.nextLink')
@@ -145,78 +146,142 @@ class OneNoteOffline:
             self.notes = dict(lib)
 
     def _find_titles_with_keyword(self, section, keyword):
-        return [title for title in self.notes[section].keys() if keyword.casefold() in title.casefold()]
+        return [
+            title for title in self.notes[section].keys()
+            if keyword.casefold() in title.casefold()
+        ]
 
     def _find_titles_with_keyword_in_page(self, section, keyword):
-        return [title for title, page in self.notes[section].items() if keyword.casefold() in page.casefold()]
+        return [
+            title for title, page in self.notes[section].items()
+            if keyword.casefold() in page.casefold()
+        ]
 
-    def display(self):
-        '''Display notes according to given parameters'''
+    def _find_sections_with_keyword(self, keyword):
+        return [
+            section_name for section_name in self.notes.keys()
+            if keyword.casefold() in section_name.casefold()
+        ]
 
-        # finds titles by keyword in page content
-        if args.find:
-            print('Following titles have  been found:')
-            for section_name in self.notes:
-                titles_with_keyword_in_page = self._find_titles_with_keyword_in_page(section_name, args.find)
-                if titles_with_keyword_in_page:
-                    print(f'##### SECTION: {section_name} #####')
-                    for title in titles_with_keyword_in_page:
-                        print(f'          TITLE: {title}')
+    def _display_titles_with_keyword_in_page(self, keyword):
+        print('Following titles have  been found:')
+        for section_name in self.notes:
+            titles_with_keyword_in_page = self._find_titles_with_keyword_in_page(section_name, args.find)
+            if titles_with_keyword_in_page:
+                print(f'##### SECTION: {section_name} #####')
+                for title in titles_with_keyword_in_page:
+                    print(f'          TITLE: {title}')
 
-        # shows all sections
-        elif args.allsections:
-            print('\n'.join(self.notes.keys()))
+    def _print_all_sections(self):
+        print('\n'.join(self.notes.keys()))
 
-        # shows all titles in section
-        elif args.alltitles and args.section:
-            print('\n'.join(self.notes[args.section].keys()))
+    def _print_all_titles_in_section(self, section_name):
+        print('\n'.join(self.notes[args.section].keys()))
 
-        # finds titles with keyword
-        elif args.title:
-            print('Following titles have been found:')
-            for section_name in self.notes:
-                titles_with_keyword = self._find_titles_with_keyword(section_name, args.title)
-                if titles_with_keyword:
-                    print(f'##### SECTION: {section_name} #####')
-                    for title in titles_with_keyword:
-                        print(f'          TITLE: {title}')
-
-        # shows specific note
-        if args.section and args.title:
-            titles_with_keyword = self._find_titles_with_keyword(args.section, args.title)
-            print(titles_with_keyword)
+    def _print_note(self, section_name, title):
+        '''Shows specific note. If more sections match it only shows
+        sections. If one sections, but more titles match, it shows all
+        notes with matching titles
+        '''
+        sections = self._find_sections_with_keyword(section_name)
+        if len(sections) == 0:
+            print('Provided section name not found.')
+        elif len(sections) == 1:
+            section_name = sections[0]
+            titles_with_keyword = self._find_titles_with_keyword(section_name, title)
+            print(f'##### SECTION: {section_name} #####')
             for title in titles_with_keyword:
-                print(f'          TITLE: {title} ##########')
-                soup = BeautifulSoup(self.notes[args.section][title], features='lxml')
+                print(f'##### TITLE: {title}')
+                soup = BeautifulSoup(self.notes[section_name][title], features='lxml')
+                print(soup.text)
+        elif len(sections) > 1:
+            print(f'Section name : {section_name}, matches more than one section: {sections}.')
+
+    def _print_titles_with_keyword(self, keyword):
+        '''Finds titles with keyword, but if only one is found, it
+        shows note
+        '''
+        print('Following titles have been found in all sections:')
+        found = []
+        for section_name in self.notes:
+            titles_with_keyword = self._find_titles_with_keyword(section_name, keyword)
+            if titles_with_keyword:
+                print(f'##### SECTION: {section_name} #####')
+                for title in titles_with_keyword:
+                    found.append({'section_name': section_name, 'title': title})
+                    print(f'          TITLE: {title}')
+
+        if len(found) == 1:
+            title = found[0]['title']
+            section = found[0]['section_name']
+            soup = BeautifulSoup(self.notes[section][title], features='lxml')
             print(soup.text)
 
+    def display_notes(self):
+        if args.find:
+            self._display_titles_with_keyword_in_page(args.find)
+        elif args.allsections:
+            self._print_all_sections()
+        elif args.section and args.alltitles:
+            self._print_all_titles_in_section(args.section)
+        if args.section and args.title:
+            self._print_note(args.section, args.title)
+        elif args.title:
+            self._print_titles_with_keyword(args.title)
 
-if __name__ == '__main__':
+def setup_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    file_handler = logging.FileHandler('logfile.log')
+    formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return logger
 
+def parse_arguments():
     arg_parser = argparse.ArgumentParser(
         description='Can download onenote notebook for specific account and read its contents after.'
     )
     arg_parser.add_argument('--user', '-u', help='Provide login like: xxxx@outlook.com to download notebook.')
     arg_parser.add_argument(
         '--title',
-        '-t', default=False,
-        help='Use alone to find keyword in titles, or with -s to show note from specific section.'
+        '-t',
+        default=False,
+        help='Find keyword in titles and display the page (if there is only one matching). '
+            +'Can use with together with -s to show page from specific section.'
     )
-    arg_parser.add_argument('--section', '-s', default=False, help='Use with -t or with --alltitles')
-    arg_parser.add_argument('--find', '-f', default=False, help='Finds titles by keyword in page content')
+    arg_parser.add_argument(
+        '--section',
+        '-s',
+        default=False,
+        help='Section can be provided together with with -t to print specific page '
+            +'or with --alltitles to print all titles in specific section'
+    )
+    arg_parser.add_argument(
+        '--find',
+        '-f',
+        default=False,
+        help='Finds all titles from all sections by keyword in page content'
+    )
     arg_parser.add_argument(
         "--alltitles",
         action="store_true",
         help='Show all titles in specific section (use with -s)'
     )
     arg_parser.add_argument("--allsections", action="store_true", help='Show all sections')
-    args = arg_parser.parse_args()
+
+    return arg_parser.parse_args()
+
+
+if __name__ == '__main__':
+
+    args = parse_arguments()
 
     if args.user:
         started_at = time.monotonic()
         onenote = OneNoteDownload(args.user)
-        for section_name, section_id in onenote.section_ids.items():
-            print(section_name, section_id)
+        for section_name, section_dict in onenote.section_ids.items():
+            print(f"Reading section: {section_name}, {section_dict['id']}")
             pages = onenote.get_pages(section_name)
             all_section_notes = {}
             for title, page_id in pages.items():
@@ -230,5 +295,4 @@ if __name__ == '__main__':
         print(f'Finished in {time_taken} minutes')
 
     else:
-        onenote_offline = OneNoteOffline()
-        onenote_offline.display()
+        OneNoteOffline().display_notes()
